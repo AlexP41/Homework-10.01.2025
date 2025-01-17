bee64239877171601ae8bdc532ff29cd98df495f
AlexPr06 <freezay00828002@gmail.com>
2025-01-17 12:43:19 +0200

    Initial commit

--- Homework 10.01.2025/Source.cpp
+++ Homework 10.01.2025/Source.cpp
@@ -1,8 +1,15 @@
 #include <iostream>  
 
+
 using namespace std; 
 
 int main() {
+    system("chcp 1251>null");
+    setlocale(LC_ALL, "uk_UA.UTF-8");
+    srand(time(NULL));
+
+    cout << "Перевірка української мови" << endl;
+
     cout << "Hello, World!" << endl;  
     return 0;  
 }
